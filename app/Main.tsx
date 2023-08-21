b0dd13b04f0ac734a93b93097139ac9fedb096f0
Prabhu Kiran Konda <prabhukirankonda@Prabhus-MacBook-Air.local>
2023-08-21 17:21:21 +0530

    modifications

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,12 +3,32 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
+import Image from 'next/image'
 
 const MAX_DISPLAY = 5
 
 export default function Home({ posts }) {
   return (
     <>
+      <div className="flex items-center justify-between space-y-4 mb-5">
+        <div>
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            <span className="wave">👋🏻</span>, <span>I'm </span>
+            <span className="text-teal-400">Prabhu Kiran Konda</span>
+          </h1>
+          <p>
+            Welcome to my blog - here I share everything that I love.
+            <br />
+          </p>
+        </div>
+        <Image
+          src="/static/images/avatar.jpg"
+          alt="avatar"
+          width={200}
+          height={200}
+          className="h-50 w-50 rounded-full shadow-lg shadow-gray-400"
+        />
+      </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
@@ -81,11 +101,11 @@ export default function Home({ posts }) {
           </Link>
         </div>
       )}
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
