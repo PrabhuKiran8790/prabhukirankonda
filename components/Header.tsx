7e98c686e5fdbe2f6064e84e3459b42dff65e2ad
Prabhu Kiran Konda <prabhukirankonda@Prabhus-MacBook-Air.local>
2023-08-21 22:28:04 +0530

    modifications

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,44 +13,46 @@ const Header = () => {
   const pathname = usePathname()
 
   return (
-    <header className="flex items-center justify-between shadow-md p-5 mt-3 bg-gray-200 dark:bg-zinc-800 rounded-lg transition-colors duration-100">
-      <div>
-        <Link href="/" aria-label={siteMetadata.headerTitle}>
-          <div className="flex items-center justify-between">
-            <div className="mr-3">
-              <Logo />
-            </div>
-            {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
-                {siteMetadata.headerTitle}
+    <div className="p-2">
+      <header className="flex items-center justify-between shadow-md p-5 mb-8 bg-zinc-200/90 dark:bg-zinc-800 rounded-lg transition-colors duration-100">
+        <div>
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              <div className="mr-3">
+                <Logo />
               </div>
-            ) : (
-              siteMetadata.headerTitle
-            )}
-          </div>
-        </Link>
-      </div>
-      <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
-        {headerNavLinks
-          .filter((link) => link.href !== '/')
-          .map((link) => (
-            <Link
-              key={link.title}
-              href={link.href}
-              className={`hidden sm:block text-black dark:text-gray-100 hover:p-2 hover:bg-gray-400/50 hover:font-extrabold hover:dark:bg-zinc-700 hover:rounded-md ${
-                pathname.startsWith(link.href)
-                  ? 'p-2 bg-gray-400/50 font-extrabold text-base dark:bg-zinc-700 rounded-md'
-                  : 'text-lg font-medium'
-              }`}
-            >
-              {link.title}
-            </Link>
-          ))}
-        <SearchButton />
-        <ThemeSwitch />
-        <MobileNav />
-      </div>
-    </header>
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
+          </Link>
+        </div>
+        <div className="flex items-center leading-5 space-x-4 sm:space-x-6">
+          {headerNavLinks
+            .filter((link) => link.href !== '/')
+            .map((link) => (
+              <Link
+                key={link.title}
+                href={link.href}
+                className={`hidden sm:block text-black dark:text-gray-100 hover:p-2 hover:bg-gray-400/50 hover:font-extrabold hover:dark:bg-zinc-700 hover:rounded-md ${
+                  pathname.startsWith(link.href)
+                    ? 'p-2 bg-gray-400/50 font-extrabold text-base dark:bg-zinc-700 rounded-md'
+                    : 'text-lg font-medium'
+                }`}
+              >
+                {link.title}
+              </Link>
+            ))}
+          <SearchButton />
+          <ThemeSwitch />
+          <MobileNav />
+        </div>
+      </header>
+    </div>
   )
 }
 
