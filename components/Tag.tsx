aa8030fac18a903a8086c479175eb1f46d8ab183
Prabhu Kiran Konda <prabhukirankonda@Prabhus-MacBook-Air.local>
2023-08-22 16:44:34 +0530

    changed tag color in dark mode

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -8,7 +8,7 @@ const Tag = ({ text }: Props) => {
   return (
     <Link
       href={`/tags/${slug(text)}`}
-      className="mr-3 mt-1 text-sm md:text-base font-bold uppercase text-blue-600 dark:text-primary-500 hover:text-zinc-900 hover:underline underline-offset-4 hover:decoration-wavy dark:hover:text-green-500"
+      className="mr-3 mt-1 text-sm md:text-base font-bold uppercase text-blue-600 dark:text-sky-500 hover:text-zinc-900 hover:underline underline-offset-4 hover:decoration-wavy dark:hover:text-green-500"
     >
       {text.split(' ').join('-')}
     </Link>
