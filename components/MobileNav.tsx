0ddaf02c2883b3158a65b07e2d3efbb7ce28011c
Prabhu Kiran Konda <prabhukirankonda@Prabhus-MacBook-Air.local>
2023-08-22 18:40:31 +0530

    changed styles in mobiile nav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import Link from './Link'
 import headerNavLinks from '@/data/headerNavLinks'
 import { usePathname } from 'next/navigation'
+import { Menu, XSquare } from 'lucide-react'
 
 const MobileNav = () => {
   const [navShow, setNavShow] = useState(false)
@@ -25,46 +26,24 @@ const MobileNav = () => {
   return (
     <>
       <button aria-label="Toggle Menu" onClick={onToggleNav} className="sm:hidden">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="text-gray-900 dark:text-gray-100 h-8 w-8"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
+        <Menu size={28} />
       </button>
       <div
         className={`fixed left-0 top-0 z-10 h-full w-full transform opacity-95 dark:opacity-[0.98] bg-white duration-300 ease-in-out dark:bg-zinc-900 ${
           navShow ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
-        <div className="flex justify-end">
-          <button className="mr-8 mt-11 h-8 w-8" aria-label="Toggle Menu" onClick={onToggleNav}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="text-gray-900 dark:text-gray-100"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
+        <div className="flex justify-end mb-5">
+          <button className="mr-8 mt-7 h-8 w-8" aria-label="Toggle Menu" onClick={onToggleNav}>
+            <XSquare size={28} />
           </button>
         </div>
-        <nav className="fixed mt-8 h-full">
+        {/* <div className="fixed mt-8 w-full space-y-1">
           {headerNavLinks.map((link) => (
             <div key={link.title} className="px-12 py-4">
               <Link
                 href={link.href}
-                className={`text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100 ${
+                className={`text-2xl font-bold w-full tracking-widest text-gray-900 dark:text-gray-100 ${
                   pathname === link.href
                     ? 'bg-gray-300 dark:bg-zinc-700/90 dark:text-green-500 p-2 rounded-md'
                     : ''
@@ -75,7 +54,24 @@ const MobileNav = () => {
               </Link>
             </div>
           ))}
-        </nav>
+        </div> */}
+
+        <div className="space-y-1 px-6 mr-4">
+          {headerNavLinks.map((route) => (
+            <Link
+              key={route.href}
+              href={route.href}
+              className={`group flex p-3 w-full justify-start cursor-pointer text-2xl font-bold tracking-widest text-gray-900 dark:text-gray-100 transition ${
+                pathname === route.href
+                  ? 'bg-gray-300 dark:bg-zinc-700/90 dark:text-green-500 p-2 rounded-md'
+                  : ''
+              }
+              `}
+            >
+              <div className="flex items-center flex-1">{route.title}</div>
+            </Link>
+          ))}
+        </div>
         <div className="fixed bottom-3 left-1/2 transform -translate-x-1/2 bg-transparent text-white p-3 w-60">
           <div className="text-center text-base">
             <div className="p-2 bg-gray-300 dark:bg-zinc-700/60 rounded-lg -ml-10">
