c4096143671f76e4ce3bf7abf6b21fdc7e1976a2
Prabhu Kiran Konda <prabhukirankonda@Prabhus-MacBook-Air.local>
2023-08-25 17:22:18 +0530

    added newsletter

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -43,7 +43,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
     // Please add your .env file and modify it according to your selection
-    provider: 'emailoctopus',
+    provider: 'mailchimp',
   },
   comments: {
     // If you want to use an analytics provider you have to add it to the
