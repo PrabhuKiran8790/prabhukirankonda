7a7e4982c65357d881af23bb3bfe2e96a8972d15
Prabhu Kiran Konda <prabhukirankonda@Prabhus-MacBook-Air.local>
2023-08-28 21:14:26 +0530

    added sticky image

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -20,7 +20,7 @@ export default function AuthorLayout({ children, content }: Props) {
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          <div className="flex flex-col items-center space-x-2 pt-8">
+          <div className="flex flex-col items-center space-x-2 pt-8 sticky top-0">
             {avatar && (
               <Image
                 src={avatar}
